92ad094cffe668b55e4505a7b31653f549746609
thecrimsoncoder <sm860129@wcupa.edu>
2018-05-19 11:14:43 -0400

    init commit

---
+++ ghostWriter.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    programTitle()
+    menu()
+
+
+
+def checkSetup():
+    return True
+
+def programTitle():
+    print("++++++++++++++++++++++++++++++++++++++++")
+    print("+     g h o s t W r i t e r . p y      +")
+    print("+     Created By: TheCrimsonCoder      +")
+    print("+     github.com/thecrimsoncoder       +")
+    print("++++++++++++++++++++++++++++++++++++++++")
+
+def menu():
+
+main()
